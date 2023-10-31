224aabbae02ce2f3b79a51816a7b29a87fc1ccf2
Alex_W <alexcwinter7@gmail.com>
2023-10-31 14:49:38 -0500

    global variables added

---
+++ guess-the-word-main/js/script.js
@@ -0,0 +1,16 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const button = document.querySelector(".guess");
+const letters = document.querySelector(".letter");
+//Variable letters is for the text input where a letter is guessed
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+// Variable remaining is where it displays the remaining guessess
+const guessesLeft = document.querySelector(".remaining span");
+//Variable guessess left is where it displays the remaining guesses
+const message = document.querySelector(".message");
+//Variable message is where a message will display when a guess is made
+const playAgain = document.querySelector(".play-again")
+//Variable playAgain is the hidden play again button
+const word = "magnolia"
+console.log(guessedLetters, button, letters, wordInProgress, remaining, guessesLeft, message, playAgain, word);
+
